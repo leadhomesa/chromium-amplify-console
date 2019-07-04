9cb28ed99fa2aedb19be859c9c77db2179a1e3fa
Seagyn Davis <seagyn.davis@leadhome.co.za>
2019-07-04 16:34:23 +0200

    Update ci.sh

--- .buildkite/ci.sh
+++ .buildkite/ci.sh
@@ -3,6 +3,8 @@
 # Init
 set -euo pipefail
 
+DOCKER_REPO="leadhome/chromium-amplify-console"
+
 # Change current branch since git is currently a detached HEAD. This is required for gitversion to work
 git checkout -B ${BUILDKITE_BRANCH}
 git reset --hard
@@ -10,10 +12,10 @@ git reset --hard
 # Run gitversion, mounting the repo root folder. Export the result as BUILD_NUMBER
 docker run -v "$(pwd):/repo" gittools/gitversion-dotnetcore:linux /repo
 export BUILD_NUMBER=$(docker run -v "$(pwd):/repo" gittools/gitversion-dotnetcore:linux /repo | jq -r ".LegacySemVer")
-export DOCKER_TAG="leadhome/chromium-amplify-console:${BUILD_NUMBER} leadhome/chromium-amplify-console:latest"
+export DOCKER_TAG="${DOCKER_REPO}:${BUILD_NUMBER} ${DOCKER_REPO}:latest"
 echo "+++ :package: Building version ${BUILD_NUMBER}"
 
 # Build docker image
 docker build -t ${DOCKER_TAG} .
 
-docker push ${DOCKER_TAG}
+docker push ${DOCKER_REPO}
